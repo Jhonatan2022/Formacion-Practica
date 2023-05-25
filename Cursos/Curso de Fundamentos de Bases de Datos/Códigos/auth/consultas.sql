bfd8be3927ff9f18e3e5fa7eaf134fd452283c4f
Jhonatan Florez <Florezj328@gmail.com>
2023-05-25 12:08:42 -0500

    Add Data In DataBase

---
+++ Cursos/Curso de Fundamentos de Bases de Datos/Códigos/auth/consultas.sql
@@ -0,0 +1,5 @@
+-- Llamamos todos los datos de la tabla posts
+SELECT * FROM posts;
+-- Fecha de publicación mayor a 2020
+SELECT * FROM posts WHERE fecha_publicacion > '2020-01-01';
+SELECT * FROM posts WHERE fecha_publicacion < '2020-01-01';
