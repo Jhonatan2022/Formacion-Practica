23cfbf54d2206b9289dcba3c4dc1282cf61e29f5
Jhonatan Florez <Florezj328@gmail.com>
2023-06-01 15:58:35 -0500

    Backup

--- Cursos/Curso Práctico de SQL y MySQL/CODE/Comandos.sh
+++ Cursos/Curso Práctico de SQL y MySQL/CODE/Comandos.sh
@@ -586,6 +586,9 @@ drop table nombre_tabla;
 show warnings;
 
 
+# Comando para eliminar el contenido de una tabla
+truncate nombre_tabla;
+
 
 # Calculamos la edad de las personas
 SELECT CONCAT("AGE:",
@@ -656,3 +659,151 @@ LEFT JOIN books AS b
         AND 5
     GROUP BY  a.author_id
 ORDER BY  a.author_id;
+
+
+
+
+
+# TRANSFORMANDO PREGUNTAS EN CONSULTAS
+# 1. ¿Qué nacionalidades hay?
+# Mediante la clausula DISTINCT trae solo los elementos distintos
+SELECT DISTINCT nationality 
+FROM authors
+ORDER BY 1;
+
+
+
+
+# 2. ¿Cuántos escritores hay de cada nacionalidad?
+#  IS NOT NULL para traer solo los valores diferentes de nulo
+#  NOT IN para traer valores que no sean los declarados (RUS y AUT)
+SELECT nationality,
+        COUNT(author_id) AS c_authors
+    FROM authors
+    WHERE nationality IS NOT NULL
+        AND nationality NOT IN ("RUS")
+    GROUP BY  nationality
+ORDER BY  c_authors DESC, nationality ASC;
+
+
+
+# 4. ¿Cuál es el promedio/desviación standard del precio de libros?
+SELECT a.nationality,
+        AVG(b.price) AS promedio,
+        STDDEV(b.price) AS std
+    FROM books AS b
+JOIN authors AS a
+    ON a.author_id = b.author_id
+    GROUP BY  a.nationality
+ORDER BY  promedio DESC;
+
+
+
+# 5. ¿Cuál es el promedio/desviación standard del precio de libros por nacionalidad?
+# Agrupar por la columna pivot
+SELECT a.nationality,
+        COUNT(b.book_id) AS libros,
+        AVG(b.price) AS promedio,
+        STDDEV(b.price) AS std
+    FROM books AS b
+JOIN authors AS a
+    ON a.author_id = b.author_id
+    GROUP BY  a.nationality
+ORDER BY  libros DESC;
+
+
+
+# 6. ¿Cuál es el precio máximo/mínimo de un libro?
+SELECT nationality,
+        MAX(price),
+        MIN(price)
+    FROM books AS b
+JOIN authors AS a
+    ON a.author_id = b.author_id
+    GROUP BY  nationality;
+
+
+
+# 7. ¿cómo quedaría el reporte de préstamos?
+# CONCAT: para concatenar en cadenas de texto.
+# TO_DAYS: recibe un timestamp ó un datetime
+SELECT c.name,
+        t.type,
+        b.title,
+        CONCAT(a.name,
+        " (",
+        a.nationality,
+        ")") AS autor,
+        TO_DAYS(NOW()) - TO_DAYS(t.created_at)
+    FROM transactions AS t
+LEFT JOIN clients AS c
+    ON c.client_id = t.client_id
+LEFT JOIN books AS b
+    ON b.book_id = t.book_id
+LEFT JOIN authors AS a
+    ON b.author_id = a.author_id;
+
+
+
+
+# eLIMINAR CON LIMITE
+DELETE
+    FROM authors
+    WHERE author_id = 161 LIMIT 1;
+
+
+
+# UPDATE basico
+UPDATE clients SET active = 0
+    WHERE client_id = 80 LIMIT 1;
+
+
+
+# 
+SELECT  sellable, SUM(price*copies) FROM books GROUP BY sellable;
+
+
+# Usanso condicionales
+SELECT COUNT(book_id),
+        SUM(IF(year < 1950,
+        1,
+        0)) AS old_books,
+        SUM(IF(year >= 1950
+        AND year < 1990, 1, 0)) AS new_books, SUM(IF(year >= 1990
+        AND year < 2000, 1, 0)) AS new_books_2000, SUM(IF(year >= 2000, 1, 0)) AS "new_books_>2000"
+    FROM books;
+
+
+
+
+# Agrupar el query anterior y mostrar su nacionalidad
+SELECT nationality,
+        COUNT(book_id),
+        SUM(IF(year < 1950,
+        1,
+        0)) AS `<1950`,
+        SUM(IF(year >= 1950
+        AND year < 1990, 1, 0)) AS `<1990`, SUM(IF(year >= 1990
+        AND year < 2000, 1, 0)) AS `<2000`, SUM(IF(year >= 2000, 1, 0)) AS `<hoy`
+    FROM books AS b
+JOIN authors AS a
+    ON a.author_id = b.author_id
+    WHERE a.nationality IS NOT NULL
+    GROUP BY  nationality;
+
+
+# Comando para alterar una tabla
+# AFTER para indicar que columna va despues de la columna indicada
+ALTER TABLE authors ADD COLUMN birthyear INTEGER DEFAULT 1930 AFTER name;
+
+
+# Comando para modificar una tabla
+ALTER TABLE authors MODIFY COLUMN birthyear year(4) DEFAULT 1920;
+
+
+# Comando para eliminar una columna de una tabla
+ALTER TABLE authors DROP COLUMN birthyear;
+
+
+# MYSQLUMP para hacer un backup de la base de datos en un archivo sql (solo estructura)
+mysqldump -u root -p --no-data database_name > database_name.sql
