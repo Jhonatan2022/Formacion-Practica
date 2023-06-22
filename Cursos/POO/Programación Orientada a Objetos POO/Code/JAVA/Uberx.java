541180bc5e69efa82fdc1e82dfc3ce6b3029a86d
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-06-22 12:39:21 -0500

    Private ne Java

--- Cursos/POO/Programación Orientada a Objetos POO/Code/JAVA/Uberx.java
+++ Cursos/POO/Programación Orientada a Objetos POO/Code/JAVA/Uberx.java
@@ -2,11 +2,19 @@
 class Uberx extends Car{
     String brand;
     String model;
+    
 
     public Uberx(String license, Account driver, String brand, String model){
         // Super hace referencia a la clase padre (Car)
         super(license, driver);
         this.brand = brand;
         this.model = model;
+    }
+
+
+    @Override
+    void printDataCar() {
+        super.printDataCar();
+        System.out.println("Modelo: " + model + " Marca: " + brand);
     }
 }
