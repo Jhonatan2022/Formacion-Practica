681513f34d8e9c0f576dfba3a1a441a1ac56142d
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-06-22 23:13:20 -0500

    Arrays And Disc

--- Cursos/POO/Fundamentos de Python/Code/Tples.py
+++ Cursos/POO/Fundamentos de Python/Code/Tples.py
@@ -1,12 +1,13 @@
 numbers = (1, 2, 3, 5)
 strings = ('nico', 'zule', 'santi', 'nico')
+
 print(numbers)
-print('0 =>', numbers[0])
-print('-1 =>', numbers[-1])
-print(type(numbers))
+print('0 =>', numbers[0]) # 0 => 1
+print('-1 =>', numbers[-1]) # -1 => 5
+print(type(numbers)) # <class 'tuple'>
 
 print(strings)
-print(type(strings))
+print(type(strings)) # <class 'tuple'>
 
 # CRUD
 # numbers.append(10)
@@ -14,15 +15,15 @@ print(numbers)
 # numbers[1] = 'change'
 
 print(strings)
-print(strings.index('zule'))
-print(strings.count('nico'))
+print(strings.index('zule')) # 1
+print(strings.count('nico')) # 2
 
-my_list = list(strings)
-print(my_list)
-print(type(my_list))
+my_list = list(strings) # Convertir tupla a lista
+print(my_list) # ['nico', 'zule', 'santi', 'nico']
+print(type(my_list)) # <class 'list'>
 
-my_list[1] = 'juli'
-print(my_list)
+my_list[1] = 'juli' # Cambiar valor de la lista
+print(my_list) # ['nico', 'juli', 'santi', 'nico']
 
-my_tuple = tuple(my_list)
-print(my_tuple)
+my_tuple = tuple(my_list) # Convertir lista a tupla
+print(my_tuple) # ('nico', 'juli', 'santi', 'nico')
