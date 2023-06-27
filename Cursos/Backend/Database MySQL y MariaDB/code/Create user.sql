2869e93a4475548d8739c89a378e838eaea3106e
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-06-27 12:19:06 -0500

    Delete User Mariadb

--- Cursos/Backend/Database MySQL y MariaDB/code/Create user.sql
+++ Cursos/Backend/Database MySQL y MariaDB/code/Create user.sql
@@ -1,7 +1,14 @@
--- Creando usuarios paramariadb
+-- Creando usuarios para mariadb
 CREATE USER
     -- Indicamos el nombre del usuario
     'soporte'@'localhost'
 IDENTIFIED BY
     -- Indicamos la contraseña del usuario
     'soporte';
+
+
+
+-- Eliminando usuarios para mariadb
+DROP USER
+    -- Indicamos el nombre del usuario
+    'soporte'@'localhost';
