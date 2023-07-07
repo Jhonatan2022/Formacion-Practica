618718a6facfbf159fe915e371515467e95f2ee5
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-07-07 14:09:48 -0500

    Models

--- Cursos/POO/FastAPI Introducción, Operaciones, Validaciones y Autenticación/app/main.py
+++ Cursos/POO/FastAPI Introducción, Operaciones, Validaciones y Autenticación/app/main.py
@@ -1,8 +1,18 @@
 # Importamos FastAPI
-from fastapi import FastAPI
+# Importamos Body para poder usar el metodo post y recibir datos en el body
+from fastapi import FastAPI, Body
 
 # Importamos htmlResponse para poder devolver código HTML
 from fastapi.responses import HTMLResponse
+
+# Importamos requests de fastapi para poder usar el metodo post
+from fastapi import Request
+
+# Importamos BaseModel para poder usar validaciones
+from pydantic import BaseModel
+
+# Importamso opional para poder usar validaciones
+from typing import Optional
 #------------------------------IMPORT THIS-------------------------------------
 
 
@@ -17,6 +27,16 @@ app.title = "My First API" # Se verá en la documentación de la API
 # Para asignar la versión de la API
 app.version = "1.0.0" # Se verá en la documentación de la API
 
+
+
+# Creamos una clase movia que hereda de BaseModel para poder usar validaciones
+class Movie(BaseModel):
+    id: Optional[int] = None # Optional para que el id sea opcional
+    title: str
+    overview: str
+    year: str
+    rating: float
+    category: str
 
 
 # Creamos el diccionario que usarémos como base de datos
@@ -71,6 +91,76 @@ def get_movie(id: int):
     
     # Si no se encuentra la película, devolvemos un mensaje
     return {'message': 'Movie not found'}
+
+
+
+
+# Parametros query
+@app.get("/movies/", tags=["Movies"])
+def get_movies_category(category: str, year: int):
+
+    # Buscamos la película por la categoría y el año
+    for movie in movies:
+        if movie['category'] == category and movie['year'] == str(year):
+            return movie
+        
+    # Si no se encuentra la película, devolvemos un mensaje
+    return {'message': 'Movie not found'}
+
+
+
+
+# Metodo POST
+@app.post("/movies", tags=["Movies"])
+def add_movie(movie: Movie):
+
+    # Agregamos la película a la base de datos
+    movies.append(movie)    
+
+    # Devolvemos la lista de películas actualizada
+    return movies
+
+
+
+
+# Metodo PUT
+@app.put("/movies/{id}", tags=["Movies"])
+def update_movie(id: int, movie: Movie):
+
+    # Buscamos la película por el id
+    for item in movies:
+        if item['id'] == id:
+            item['title'] = movie.title
+            item['overview'] = movie.overview
+            item['year'] = movie.year
+            item['rating'] = movie.rating
+            item['category'] = movie.category
+
+
+            # Devolvemos el id de la pelicula actualizada y la lista de películas actualizada
+            return {'message': 'Success Update','id': id, 'movies': movies}
+    
+    # Si no se encuentra la película, devolvemos un mensaje
+    return {'message': 'Movie not found'}
+
+
+
+
+# Metodo DELETE
+@app.delete("/movies/{id}", tags=["Movies"])
+def delete_movie(id: int):
+
+    # Buscamos la película por el id
+    for movie in movies:
+        if movie['id'] == id:
+            movies.remove(movie)
+
+            # Devolvemos un mensaje de confirmación con las películas actualizadas
+            return {'message': 'Movie deleted', 'movies': movies}
+    
+    # Si no se encuentra la película, devolvemos un mensaje
+    return {'message': 'Movie not found'}
+
 
 
 
